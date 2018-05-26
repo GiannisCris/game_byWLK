bd56f567c6e4a9726ed3c8d67b8e1872cd8a8278
ChrisTree <cuicuitrees@gmail.com>
2018-05-26 21:07:36 +0800

    提交

--- src/GSME.java
+++ src/GSME.java
@@ -6,9 +6,9 @@ public class GSME extends JFrame{
 	boolean right=true;
 	Image ball=Toolkit.getDefaultToolkit().getImage("pic/2.png");
 	Image desk=Toolkit.getDefaultToolkit().getImage("pic/1.png");
-	//画窗口
+
 	public void paint(Graphics g) {
-		System.out.println("被画了一次");
+		System.out.println("锟斤拷锟斤拷锟斤拷一锟斤拷");
 		g.drawImage(desk,10,20,null);
 		g.drawImage(ball,x,y,null);
 		if(right)
@@ -20,13 +20,13 @@ public class GSME extends JFrame{
 		{right=false;}
 			
 	}
-	//加载窗口
+
 	void launchFrame() {
 	setSize(405,250);
 	setLocation(150,150);
 	setVisible(true);
 	
-	//重新画窗口
+
 	while(true) {
 		repaint();
 		try {
